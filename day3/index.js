26c8cf08a62ae1614d887a00994c9a3247712298
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-04-14 00:57:22 +0600

    day three of js

---
+++ day3/index.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 20;
+
+console.log(a + b); // 30
+console.log(a - b); // -10
+console.log(b - a); // 10
+console.log(a * b); // 200
+console.log(a / b); // 0.5
+
+let count = 5;
+
+console.log(count++);
+console.log("====================================");
+console.log(count);
+console.log("====================================");
+
+console.log("cow" && "horse");
