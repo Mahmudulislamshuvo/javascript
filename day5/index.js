2ce27422e721d14dde03908ffd9bbad02ef60b59
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-04-17 14:23:08 +0600

    day 5 almst complete

--- day5/index.js
+++ day5/index.js
@@ -1,12 +1,47 @@
-var Storm = function () {};
-Storm.prototype.precip = "rain";
+// piramid
+let star = "*";
 
-var WinterStorm = function () {};
-WinterStorm.prototype = new Storm();
-WinterStorm.prototype.precip = "snow";
+for (let num = 1; num <= 10; num++) {
+  console.log(star);
+  star += "*";
+}
 
-var bob = new WinterStorm();
-console.log(bob.precip);
+// Multiplication
+let n = 3;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${n} * ${i} = ${n * i}`);
+}
 
-let x = 0 && "lws";
-console.log(x);
+// Find the summation of all odd numbers between 1 to 500 and print them on teh console log.
+let sum = 0;
+for (let n = 1; n <= 500; n++) {
+  if (n % 2 !== 0) {
+    sum += n;
+  }
+}
+console.log(sum);
+
+// 4. Skipping Multiples of 3
+for (let n = 1; n <= 20; n++) {
+  if (n % 3 === 0) continue;
+  console.log(n);
+}
+
+// task 5
+
+let number = 6789;
+let numConvertTostr = number.toString();
+let reversedStr = "";
+let i = numConvertTostr.length - 1;
+
+while (i >= 0) {
+  reversedStr += numConvertTostr[i];
+  i--;
+}
+// এটাকে নাম্বারে দেখাতে সুধু +reversedStr এভাবে লগ করলেই নাম্বার আসবে।
+console.log(reversedStr);
+
+// চেকিং হচ্ছে i কি ০ এর থেকে বড় কিনা, বড় হলে একবার লুপ ঘুরতেছে,
+// শেষ লাইনে বলা হয়েছে i এর মান ১ কমিয়ে দাও। এভাবে করে যখন i=০ হয়ে যাচ্ছে তখন লুপ বন্ধ হয়ে যাচ্ছে।
+
+// Task -6
