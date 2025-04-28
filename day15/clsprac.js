2e13200ee85391ce3a5bf41f68faefbb881df514
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-04-29 02:17:45 +0600

    array huge method learning

--- day15/clsprac.js
+++ day15/clsprac.js
@@ -88,3 +88,85 @@ console.log(arr2.length);
 
 arr1.length = 2 ** 32 - 1;
 console.log(arr1); // 4294967295
+
+const arrr1 = [1, 3, 5];
+const arrr2 = [2, 4, 6];
+
+const mergeArray = arrr1.concat(arrr2);
+console.log(mergeArray);
+
+const numberrs = [1, 3, 5, 7, "ujg"];
+const jined = numberrs.join();
+console.log(jined);
+
+console.log([].join()); // return ""
+
+// fill method
+
+// const colors = ["red", "blue", "green", "golden"];
+// console.log(colors);
+// colors.fill("pink");
+// console.log(colors);
+
+const colors = ["red", "blue", "green", "golden"];
+
+colors.fill("blue", 1, 3);
+
+console.log(colors);
+
+const names = ["tom", "sumona", "mog", "shuvo"];
+console.log(names.includes("sumona"));
+console.log(names.includes("june"));
+
+console.log(names.indexOf("sumona")); //it will show index number from LEFT TO RIGHT
+console.log(names.lastIndexOf("shuvo")); //RIGHT TO LEFT
+
+console.log(names.reverse());
+
+const namess = [
+  "Sumona Akter",
+  "Shuvo Hossain",
+  "Rakib Hasan",
+  "Mim Sultana",
+  "Jannat Ara",
+  "Hasan Mahmud",
+  "Tania Akter",
+];
+
+console.log(namess.sort());
+
+const num = [5, 23, 10, 38, 97, 868];
+console.log(num.sort());
+num.sort(function (a, b) {
+  return a - b;
+});
+
+// splice mrthod
+const usaNames = [
+  "Emily",
+  "James",
+  "Michael",
+  "Olivia",
+  "Ethan",
+  "Sophia",
+  "Daniel",
+  "Ava",
+  "Matthew",
+  "Isabella",
+];
+// at
+console.log(usaNames.at(1));
+console.log(usaNames.at(-1));
+console.log(usaNames.at(-4));
+
+const upadtedSplice = usaNames.splice(0, 2, "shuvo");
+console.log(usaNames);
+
+// copywithin
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.copyWithin(0, 3, 5);
+console.log(numbers); // [4, 5, 3, 4, 5, 6, 7, 8, 9]
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+array.copyWithin(0, 3, 6);
+console.log(array); //[4, 5, 6, 4, 5, 6, 7];
